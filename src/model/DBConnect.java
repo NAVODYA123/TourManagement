a4493e498b52d99674c85bf79a6c73a8adcd6ede
GayanKalinga <bg15407@gmail.com>
2017-05-10 09:12:20 +0530

    DBConnect Class code optimized introduced new variables to handle the Strings

--- src/model/DBConnect.java
+++ src/model/DBConnect.java
@@ -7,15 +7,26 @@ import java.sql.DriverManager;
 
 
 public class DBConnect {
+
     public static Connection dbConnection(){
+
         Connection con= null;
+        String sqlDriver = "com.mysql.jdbc.Driver";
+        String url = "jdbc:mysql://localhost:3306/tour?autoReconnect=true&useSSL=false";
+        String sqlUserName = "root";
+        String sqlPassword = "";
+
         try {
-            Class.forName("com.mysql.jdbc.Driver"); //.newInstance();
-            con = DriverManager.getConnection("jdbc:mysql://localhost:3306/tour?autoReconnect=true&useSSL=false", "root", "");
+                //Introducing the Driver to the Class
+            Class.forName(sqlDriver); //.newInstance();
+            con = DriverManager.getConnection(url, sqlUserName, sqlPassword);
 
         }catch (Exception e){
+
             System.out.println(e);
+
         }
+
         return   con;
 
     }
